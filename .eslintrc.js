6de3c01ac715b9190cbd5759e1b16ee0efcfd7c5
Adam <adam@haglund.dev>
2023-06-14 17:08:28 +0200

    update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 /** @type import("eslint-define-config").ESLintConfig */
 module.exports = {
   root: true,
-  ignorePatterns: ["*eslintrc*"],
+  ignorePatterns: ["*eslintrc*", "**/fixtures/**", "**/__snapshots__/**"],
   extends: ["plugin:@beequeue/base", "plugin:@beequeue/typescript"],
   rules: {
     "@typescript-eslint/no-non-null-assertion": "off",
