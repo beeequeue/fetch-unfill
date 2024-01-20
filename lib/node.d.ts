dd7e04301728e1b22607092274f048e08a9b6dcd
Adam <adam@haglund.dev>
2024-01-20 19:35:32 +0100

    use undici-types instead of node-fetch's

--- lib/node.d.ts
+++ lib/node.d.ts
@@ -1,1 +1,8 @@
-export * from "node-fetch"
+import type { fetch as $fetch, $Headers, $Request, $Response } from "undici-types"
+
+declare global {
+  const fetch: typeof $fetch
+  const Headers: typeof $Headers
+  const Request: typeof $Request
+  const Response: typeof $Response
+}
