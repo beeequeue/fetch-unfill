8dfbbcb3fefec033c0c4271dcd7da32069a8793d
Adam <adam@haglund.dev>
2024-01-20 20:58:50 +0100

    add AbortController

--- lib/node.d.ts
+++ lib/node.d.ts
@@ -1,8 +1,15 @@
-import type { fetch as $fetch, $Headers, $Request, $Response } from "undici-types"
+import type {
+  fetch as $fetch,
+  $Headers,
+  $Request,
+  $Response,
+  AbortController as $AbortController,
+} from "undici-types"
 
 declare global {
   const fetch: typeof $fetch
   const Headers: typeof $Headers
   const Request: typeof $Request
   const Response: typeof $Response
+  const AbortController: typeof $AbortController
 }
