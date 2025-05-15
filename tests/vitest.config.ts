2fbeb5fd7ef3def9819e752af29cfdae066b2b36
Adam <adam@haglund.dev>
2025-05-15 20:31:20 +0200

    fix ci

--- tests/vitest.config.ts
+++ tests/vitest.config.ts
@@ -1,13 +1,7 @@
-import { execSync } from "node:child_process"
-
 import { defineConfig } from "vitest/config"
 
-const gitSha = execSync("git rev-parse --short HEAD").toString().trim()
 export default defineConfig({
   test: {
     setupFiles: "vitest.setup.ts",
-    env: {
-      GIT_SHA: JSON.stringify(gitSha),
-    },
   },
 })
